63e23fd219408f6f6663d4f4f135822d3b8129f9
Daiki Tomioka <chocolate0785lego@icloud.com>
2022-06-05 13:14:37 +0900

    add TinyGPS++

--- BEP_Tomix2022/src/main.cpp
+++ BEP_Tomix2022/src/main.cpp
@@ -1,10 +1,20 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+
+#define interruptPin 2
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+void triger() {
+    Serial.print("triger\n");
+}
 
 void setup() {
+    pinMode(interruptPin, INPUT_PULLUP);
     Serial.begin(115200);
+    mySerial.begin(9600);
+    attachInterrupt(digitalPinToInterrupt(interruptPin), triger, FALLING);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    Serial.println("Hello World!");
+    if (mySerial.available()) Serial.write(mySerial.read());
 }
