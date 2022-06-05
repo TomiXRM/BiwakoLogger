d3d9ffa65640d3440fee088fa516b33565762128
Daiki Tomioka <chocolate0785lego@icloud.com>
2022-06-05 14:10:17 +0900

    GPSの受信に成功した

--- BEP_Tomix2022/src/main.cpp
+++ BEP_Tomix2022/src/main.cpp
@@ -3,25 +3,75 @@
 #include <TinyGPS++.h>
 #include <SD.h>
 
-#define interruptPin 2
+/*
+GPIO pinmap:
+D2  - GPS 1PPS
+D4  - SD_CS
+D5  - as GPS receive
+D6  - as GPS transmit
+D11 - MOSI
+D12 - as MISO
+D13 - as SCK
+*/
 
-SoftwareSerial gps(10, 11); // RX, TX
+#define interruptPin 2
+#define chipSelect 4
+
+SoftwareSerial gps(5, 6); // RX, TX
 TinyGPSPlus tinyGPS;
 
+typedef struct {
+    double latitude;
+    double longitude;
+} gpsData;
+
+gpsData GPSData;
+
+/* ----- prototype functions ----- */
+void saveGPSData(gpsData &data);
+void triger();
+void initSD();
+
+void saveGPSData(gpsData &data) {
+    File dataFile = SD.open("datalog.txt", FILE_WRITE);
+    if (dataFile) {
+        String dataString = String(data.latitude) + ',' + String(data.longitude) + '\n';
+        dataFile.println(dataString);
+    } else {
+        Serial.println(F("error opening datalog.txt"));
+    }
+    dataFile.close();
+}
+
 void triger() {
-    Serial.print(tinyGPS.location.lat(), 6);
+    GPSData = {tinyGPS.location.lat(), tinyGPS.location.lng()};
+    saveGPSData(GPSData);
+
+    Serial.print(GPSData.latitude, 6);
     Serial.print(", ");
-    Serial.println(tinyGPS.location.lng(), 6);
+    Serial.println(GPSData.latitude, 6);
+}
+
+void initSD() {
+    Serial.print(F("Initializing SD card..."));
+    if (!SD.begin(chipSelect)) {
+        while (1) {
+            delay(1000);
+            Serial.println(F("Card failed, or not present"));
+        }
+    }
+    Serial.println("SD init done");
 }
 
 void setup() {
     pinMode(interruptPin, INPUT_PULLUP);
     Serial.begin(115200);
     gps.begin(9600);
+    initSD();
     attachInterrupt(digitalPinToInterrupt(interruptPin), triger, FALLING);
 }
 
 void loop() {
-    while (gps.available())
+    if (gps.available())
         tinyGPS.encode(gps.read());
 }
